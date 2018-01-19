edea82feb50e9c80bf38bbadef4a6d4f2d48d5d5
GoranBT <goran.angelovski.bt@gmail.com>
2018-01-19 01:29:38 +0000

    Add basic rules and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+const path =require('path');
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/index.js',
   output: {
     path: '',
     filename: 'bundle.js',
-    publicPath: ''
-  }
+    publicPath: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: [
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }
+  ]
 };
